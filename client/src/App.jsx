6d5c7e6501b0af6552c7b9c0951d6c38366bd099
minh2042002 <ducminh2042002@gmail.com>
2024-06-06 00:47:23 +0700

    add stadium manager ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import './style.scss';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./style.scss";
 
-import Layout from './pages/Layout'
-import Schedule from './pages/Schedule'
+import Layout from "./pages/Layout";
+import Schedule from "./pages/Schedule";
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <BrowserRouter>
+    return (
+        <BrowserRouter>
             <Routes>
-                <Route path='/'>
-                    <Route
-                        path='/'
-                        element={
-                                <Layout />
-                        }
-                    >
-                  
-                        <Route index path='/' element={<Schedule />} />
+                <Route path="/">
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Schedule />} />
                         {/* <Route index path='recipe' element={<Recipe />} />
                         <Route index path='fridge' element={<Fridge />} />
                         <Route index path='statistic' element={<Statistic />} />
@@ -29,12 +23,11 @@ function App() {
                         <Route index path='fridge/category' element={<Category />} />
                         <Route index path='fridge/history' element={<History />} />
                         <Route index path='account-manage' element={<Account />} /> */}
-                      
                     </Route>
                 </Route>
             </Routes>
         </BrowserRouter>
-  )
+    );
 }
 
-export default App
+export default App;
